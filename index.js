5b92000f9278ed7b4ce99c4b7df264300ebba03b
Anjali Gupta <anjaligupta0621@gmail.com>
2023-09-14 01:41:33 -0400

    user authentication backend completed

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = 3000;
 const {mongoUrl} = require('./keys');
 
 require('./models/User');
+const requireToken = require('./middleware/requireToken'); 
+
 const authRoutes = require('./routes/authRoutes');
 app.use(bodyParser.json());
 app.use(authRoutes);
@@ -27,6 +29,10 @@ app.post('/', (req, res) => {
     console.log(req.body);
     res.send('Hello World!');
 });
+
+app.get('/', requireToken, (req, res) => {
+    res.send("Your email is " + req.user.email);
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const {jwtkey} = require('../keys')
 const router = express.Router();
 const User = mongoose.model('User');
 
@@ -9,12 +11,32 @@ router.post('/signup', async (req, res) => {
         try {
             const user = new User({email, password});
             await user.save();
-            res.send('Sign up successful!');
+            const token = jwt.sign({userId: user._id}, jwtkey);
+            res.send({token});
+
         } catch(err) {
-            res.status(422).send(err.message);
+            return res.status(422).send(err.message);
         }
 
         
+})
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).send({error: "Must provide email and password"});
+    }
+    const user = await User.findOne({email});
+    if (!user) {
+        return res.status(422).send({error: "Must provide email and password"});
+    }
+    try {
+        await user.comparePassword(password);
+        const token = jwt.sign({userId: user._id}, jwtkey);
+        res.send({token});
+    } catch(err) {
+        return res.status(422).send({error: "Must provide valid email and password"});
+    }
 })
 
 module.exports = router;
