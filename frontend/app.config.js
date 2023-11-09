e998d0888ced38b8b8c9f9f78b018931520ddde5
Anjali Gupta <anjaligupta0621@gmail.com>
2023-11-09 15:13:07 -0500

    config name changed

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,7 +1,7 @@
 export default ({ config }) => ({
     ...config,
-    name: "Storybook Tutorial Template",
-    slug: "storybook-tutorial-template",
+    name: "frontend",
+    slug: "frontend",
     extra: {
       storybookEnabled: process.env.STORYBOOK_ENABLED,
     },
